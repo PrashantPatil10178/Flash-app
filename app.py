6df1b4795039fab9d76e2b0620ae3382cbea6170
Prashant Patil <patilanilprashant178@gmail.com>
2025-05-12 18:07:40 +0530

    added new

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def register():
     return f"User Registered Successfully!<br>Name: {name}<br>Email: {email}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=4000)
